a55f0dd95551f558eb9f1b06120b552bea493069
Péter Kalicz <kaliczp@gmail.com>
2023-03-08 14:11:03 +0100

    Column selectable by parameter in imported file
    xlscolumn parameter is passed to read_excel()

--- beolvas.R
+++ beolvas.R
@@ -1,13 +1,13 @@
 filenames <- dir("Adatok")
 library(readxl)
 
-import.Boreas.xls <- function(filename, sheet) {
+import.Boreas.xls <- function(filename, sheet, xlscolumn = 2) {
     require(readxl)
     require(xts)
     rawdata <- as.data.frame(read_excel(filename, sheet))
     full.date <- seq.POSIXt(rawdata[1,1], rawdata[nrow(rawdata),1], by = "10 min")
     if(length(full.date) == nrow(rawdata)) {
-        xts(rawdata[,2],full.date)
+        xts(rawdata[, xlscolumn],full.date)
     } else {
         stop("Different length of databases")
     }
