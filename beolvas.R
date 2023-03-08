c37f8ef3b700f81efa74c7ecb50b994a92f7781b
Péter Kalicz <kaliczp@gmail.com>
2023-03-08 14:01:13 +0100

    Time series generation with function
    After import a function creates time stamps (first and last date-time)  and check length of raw data and generated time stamp length.

--- beolvas.R
+++ beolvas.R
@@ -1,4 +1,22 @@
 filenames <- dir("Adatok")
 library(readxl)
 
-BorArbor <- as.data.frame(read_excel(paste0("Adatok/", filenames[1]), sheet = "Arborétum"))
+import.Boreas.xls <- function(filename, sheet) {
+    require(readxl)
+    require(xts)
+    rawdata <- as.data.frame(read_excel(filename, sheet))
+    full.date <- seq.POSIXt(rawdata[1,1], rawdata[nrow(rawdata),1], by = "10 min")
+    if(length(full.date) == nrow(rawdata)) {
+        xts(rawdata[,2],full.date)
+    } else {
+        stop("Different length of databases")
+    }
+}
+
+BorArbor <- import.Boreas.xls(filename = paste0("Adatok/", filenames[1]),
+                              sheet = "Arborétum"
+                              )
+
+BorArbor <- import.Boreas.xls(filename = paste0("Adatok/", filenames[3]),
+                              sheet = "Arborétum"
+                              )
