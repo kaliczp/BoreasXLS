2b8568b8753d1f9ca2c7235f356ba3673e3ba400
Péter Kalicz <kaliczp@gmail.com>
2023-03-09 07:18:12 +0100

    Date mishmatch solved with merge
    In the case of irregularities in equidistant time measurement only
    a warning droppend instead stop.
    
    Merge replace with NA all missing timestamps. Additional measurements
    are not treated.

--- beolvas.R
+++ beolvas.R
@@ -8,11 +8,12 @@ import.Boreas.xls <- function(filename, sheet, xlscolumn = 2) {
     require(xts)
     rawdata <- as.data.frame(read_excel(filename, sheet))
     full.date <- seq.POSIXt(rawdata[1,1], rawdata[nrow(rawdata),1], by = "10 min")
-    if(length(full.date) == nrow(rawdata)) {
-        xts(rawdata[, xlscolumn],full.date)
-    } else {
-        stop("Different length of databases")
+    if(length(full.date) != nrow(rawdata)) {
+        warning("Different length of databases")
     }
+    raw.xts <- xts(rawdata[, xlscolumn], rawdata[, 1])
+    generated.xts <- xts(rep(NA, length(full.date)), full.date)
+    merge(raw.xts, generated.xts)[,1]
 }
 
 BorArbor <- import.Boreas.xls(filename = paste0("Adatok/", filenames[1]),
