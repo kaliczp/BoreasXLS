f03352a9b1ea4c99e91558cf247eedd069d17979
Péter Kalicz <kaliczp@gmail.com>
2023-03-15 20:31:01 +0100

    A solution to daylight saving issue

--- beolvas.R
+++ beolvas.R
@@ -1,4 +1,5 @@
-Sys.setenv(TZ="UTC")
+Sys.setenv(TZ="CET")
+Sys.setenv(TZ = "Europe/Budapest") # Windows?
 
 filenames <- dir("Adatok", pattern = "SoE")
 library(readxl)
@@ -7,11 +8,13 @@ import.Boreas.xls <- function(filename, sheet, xlscolumn = 2) {
     require(readxl)
     require(xts)
     rawdata <- as.data.frame(read_excel(filename, sheet))
-    full.date <- seq.POSIXt(rawdata[1,1], rawdata[nrow(rawdata),1], by = "10 min")
+    rawdatetime <- as.character(rawdata[, 1])
+    rawdatetime <- as.POSIXct(rawdatetime)
+    full.date <- seq.POSIXt(rawdatetime[1], rawdatetime[length(rawdatetime)], by = "10 min")
     if(length(full.date) != nrow(rawdata)) {
         warning("Different length of databases")
     }
-    raw.xts <- xts(rawdata[, xlscolumn], rawdata[, 1])
+    raw.xts <- xts(rawdata[, xlscolumn], rawdatetime)
     generated.xts <- xts(rep(NA, length(full.date)), full.date)
     merge(raw.xts, generated.xts)[,1]
 }
