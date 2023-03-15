3f767d393e2dedbf2066cbfe039c9513957591f1
Péter Kalicz <kaliczp@gmail.com>
2023-03-15 18:10:02 +0100

    Identify and list time problems

--- vizsgal.R
+++ vizsgal.R
@@ -1,3 +1,10 @@
 which(diff(index(BorArbor)) != 10)
-BorArbor[36090:36100]
-BorArbor[59240:59250]
+BorArbor['2021-03-27 09:00/2021-03-27 09:10'] # 09:01 additional
+BorArbor['2021-03-28 01:50/2021-03-28 03:10'] # Missing 02:00 – 03:20
+BorArbor['2021-04-18 11:00/2021-04-18 11:10'] # 11:01 additional
+BorArbor['2021-07-15 17:50/2021-07-15 18:10'] # 18:01 additional
+BorArbor['2021-09-07 06:50/2021-09-07 07:10'] # 07:01 additional
+BorArbor['2022-03-27 01:50/2022-03-27 03:10'] # Missing 02:00 – 03:20
+BorArbor['2022-09-08 15:30/2022-09-08 15:40'] # 15:31 additional
+BorArbor['2023-02-16 09:30/2023-02-16 09:40'] # 09:31 additional
+
