0ffb1f2528cb5f7f53462cf691fd3ca6c2a16e6c
Kalicz Péter <kalicz.peter@uni-sopron.hu>
2024-01-30 15:20:03 +0100

    Optional date conversion in import
    Raw date is converted to ASCII not uses time, so the result erroneous

--- beolvas.R
+++ beolvas.R
@@ -1,15 +1,19 @@
-Sys.setenv(TZ="CET")
+Sys.setenv(TZ="UTC")
 Sys.setenv(TZ = "Europe/Budapest") # Windows?
 
 filenames <- dir("Adatok", pattern = "SoE")
 library(readxl)
 
-import.Boreas.xls <- function(filename, sheet, xlscolumn = 2) {
+import.Boreas.xls <- function(filename, sheet, xlscolumn = 2, convertdate = TRUE) {
     require(readxl)
     require(xts)
     rawdata <- as.data.frame(read_excel(filename, sheet))
+    if(convertdate) {
     rawdatetime <- as.character(rawdata[, 1])
     rawdatetime <- as.POSIXct(rawdatetime)
+    } else {
+        rawdatetime <- rawdata[, 1]
+    }
     full.date <- seq.POSIXt(rawdatetime[1], rawdatetime[length(rawdatetime)], by = "10 min")
     if(length(full.date) != nrow(rawdata)) {
         warning(paste("Generated and measured timestamps are differ in", filename))
@@ -20,12 +24,12 @@ import.Boreas.xls <- function(filename, sheet, xlscolumn = 2) {
 }
 
 BorArbor <- import.Boreas.xls(filename = paste0("Adatok/", filenames[1]),
-                              sheet = "Arborétum"
+                              sheet = "Arborétum", convertdate = FALSE
                               )
 for(tti in 2:length(filenames)) {
     print(tti)
     BorArbor <- c(BorArbor, import.Boreas.xls(filename = paste0("Adatok/", filenames[tti]),
-                                              sheet = "Arborétum"
+                                              sheet = "Arborétum", convertdate = FALSE
                                               )
                   )
 }
